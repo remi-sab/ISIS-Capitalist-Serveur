b14b55cc5d8c55df824db019503fac46c2e040ff
remis <remis@172.16.18.243>
2020-03-24 17:50:45 +0100

    MAJ 24/03/2020

--- src/main/java/com/isis/generated/Webservice.java
+++ src/main/java/com/isis/generated/Webservice.java
@@ -10,8 +10,13 @@ package com.isis.generated;
  * @author remis
  */
 
+import com.google.gson.Gson;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -29,9 +34,17 @@ public class Webservice {
     @GET
     @Path("world")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})    
-    public Response getXml(@Context HttpServletRequest request) throws JAXBException{
+    public Response getXml(@Context HttpServletRequest request) throws JAXBException,FileNotFoundException, IOException{
         String username;
         username = request.getHeader("X-user");
-        return Response.ok(services.readWorldFromXml(username)).build();
+        return Response.ok(services.getWorld(username)).build();
+    }
+    
+    @PUT
+    @Path("product")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response editProduct(String data) throws JAXBException, FileNotFoundException, IOException{
+        ProductType product = new Gson().fromJson(data, ProductType.class);
+        return Response.ok(services.updateProduct(data, product)).build();
     }
 }
